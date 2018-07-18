74f0e62d083f9d5f314f4b5b9c456a90dcaa405c
rathor-kk <rathor.kk@gmail.com>
2018-07-18 10:12:07 +0530

    Creating class using Resource, Component and Other Annotations

--- src/main/java/Circle.java
+++ src/main/java/Circle.java
@@ -1,23 +1,37 @@
 package main.java;
 
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
+@Component
 public class Circle implements main.java.Shape {
 
-    private main.java.Point Center;
+    private main.java.Point center;
 
     public main.java.Point getCenter() {
-        return Center;
+        return center;
     }
 
-    @Required
+    @Resource
     public void setCenter(main.java.Point center) {
-        Center = center;
+        this.center = center;
     }
 
+    @PostConstruct
+    public void initialize(){
+        System.out.println("Init method called using annotation");
+    }
 
+    @PreDestroy
+    public void destroy(){
+        System.out.println("Init method called using annotation");
+    }
 
     public void draw() {
-        System.out.println("Circle drawn : \nPoint is (" + Center.getX() + ", " + Center.getY() + " )");
+        System.out.println("Circle drawn : \nPoint is (" + center.getX() + ", " + center.getY() + " )");
     }
 }
